5d2b0f6ffeeec1efd57b8d43a9fc28b0b713e55f
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-06-04 01:33:56 +0200

    Initial Commit

---
+++ UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Cuenta Personal on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
